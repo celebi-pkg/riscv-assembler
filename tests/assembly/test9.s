5fe3426a4381a24926a39fac7c6058dfdb685042
gongjixaing <1485049945@qq.com>
2024-09-20 11:13:09 +0800

    Add the test file test9.s to test the newly added instructions. The specific content format stored is: instruction + correct answer, and it is not registered in test_class.py.

---
+++ tests/assembly/test9.s
@@ -0,0 +1,6 @@
+fcvt.s.wu f1 x1 0xd010f0d3
+fcvt.d.wu f1 x1 0xd210f0d3
+fcvt.s.d f1 f2  0x401170d3
+fmadd.s f1, f2, f3, f4 0x203170c3
+fmadd.d f1, f2, f3, f4 0x223170c3
+amoadd.d x1, x2, (x3) 0x0021b0af
